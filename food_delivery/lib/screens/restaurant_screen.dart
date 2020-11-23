ac06c149fbaf71bcf3868969ddf68f4b5b55438f
Cihangir Ozmus <cozmus@gmail.com>
2020-11-23 10:50:03 +0300

    restaurant_screen has started.

---
+++ food_delivery/lib/screens/restaurant_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_delivery/models/restaurant.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  final Restaurant restaurant;
+
+  RestaurantScreen({this.restaurant});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
