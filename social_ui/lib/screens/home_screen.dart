8b4b059296655125242639ab0c3429d8ea68b17d
Cihangir Ozmus <cozmus@gmail.com>
2020-11-28 13:05:11 +0300

    CustomDrawer is added.

--- social_ui/lib/screens/home_screen.dart
+++ social_ui/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:social_ui/data/data.dart';
+import 'package:social_ui/widgets/custom_drawer.dart';
 import 'package:social_ui/widgets/following_users.dart';
 import 'package:social_ui/widgets/posts_carousel.dart';
 
@@ -55,6 +56,9 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
             ),
           ],
         ),
+      ),
+      drawer: CustomDrawer(
+
       ),
       body: ListView(
         children: [
