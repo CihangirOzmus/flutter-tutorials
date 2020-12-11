4f8410294947d63a2b3599e02e8074d7225951db
Cihangir Ozmus <cozmus@gmail.com>
2020-12-11 14:24:21 +0300

    ChartBar is added.

--- personal_expenses/lib/widgets/chart.dart
+++ personal_expenses/lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:personal_expenses/models/transaction.dart';
+import 'package:personal_expenses/widgets/chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
@@ -18,14 +19,21 @@ class Chart extends StatelessWidget {
       for (int i = 0; i < recentTransactions.length; i++) {
         if (recentTransactions[i].date.day == weekDay.day &&
             recentTransactions[i].date.month == weekDay.month &&
-            recentTransactions[i].date.year == weekDay.year
-        ) {
+            recentTransactions[i].date.year == weekDay.year) {
           totalSum += recentTransactions[i].amount;
         }
       }
 
-      return {"day": DateFormat.E().format(weekDay), "amount": totalSum};
+      return {
+        "day": DateFormat.E().format(weekDay).substring(0, 1),
+        "amount": totalSum
+      };
     });
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(
+        0.0, (sum, item) => sum + item['amount']);
   }
 
   @override
@@ -34,7 +42,12 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return ChartBar(
+              data['day'], data['amount'],
+              totalSpending == 0.0 ? 0.0 : (data['amount'] as double) / totalSpending
+          );
+        }).toList(),
       ),
     );
   }
